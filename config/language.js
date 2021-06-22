5c29a3348c8b4fa0fd323022dc55f91ae9a4febb
Tin Ko Hein <tinkohein.oo@gmail.com>
2021-06-22 21:24:35 +0630

    Update language.js

--- config/language.js
+++ config/language.js
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-const allowedLanguages = ['en', 'mon']
+const allowedLanguages = ['en-Us', 'mon']
 
 const defaultLanguage = 'mon'
 let lng = defaultLanguage
